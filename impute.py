ad2c97ec024f1f53467f7cd391217fb9e324ddc8
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-03-14 20:57:45 -0400

    Created a file for the impute functions

---
+++ impute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""
+Script to produce a model for imputing missing values
+in the sample partial file. Will return the imputed values
+and the most significant predictors with their coefficients
+"""
+
+import numpy as np
